0149345a8491f0e1008a5a5b514a644baa37d52d
tleseney <leseney@gmail.com>
2015-10-05 15:29:58 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,7 +16,7 @@
 		defaultFeeds	= [556036846], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Watteco LoRa', // Replaces Xively logo in the header
 		dataDuration	= '1week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 600, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
